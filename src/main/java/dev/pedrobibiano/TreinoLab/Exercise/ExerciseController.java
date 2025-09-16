d2e29886842892c80bb15a5b41714189090aaeb2
PedroBibiano <pedrobibiano777@gmail.com>
2025-09-15 23:19:48 -0300

    feat: mapeando as primeiras rotas

--- src/main/java/dev/pedrobibiano/TreinoLab/Exercise/ExerciseController.java
+++ src/main/java/dev/pedrobibiano/TreinoLab/Exercise/ExerciseController.java
@@ -1,4 +1,16 @@
 package dev.pedrobibiano.TreinoLab.Exercise;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class ExerciseController {
+
+    @GetMapping("/exercicio")
+    public String Exercise(){
+        return "exercicios";
+    }
+
 }
--- src/main/java/dev/pedrobibiano/TreinoLab/User/UserController.java
+++ src/main/java/dev/pedrobibiano/TreinoLab/User/UserController.java
@@ -1,4 +1,16 @@
 package dev.pedrobibiano.TreinoLab.User;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class UserController {
+
+    @GetMapping("/usuario")
+    public String UserController(){
+        return  "usuarios";
+    }
+
 }
